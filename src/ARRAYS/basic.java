1bf9865220fb576ddebb90721a4b3ff1d92b19dd
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-10 21:08:54 +0530

    array basics

--- src/ARRAYS/basic.java
+++ src/ARRAYS/basic.java
@@ -1,5 +1,18 @@
 package ARRAYS;
-
+import java.util.*;
 public class basic {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size=sc.nextInt();
+		//array declaration and initialize size
+		int[] arr = new int[size];
+		//array insertion
+		for(int i=0;i<arr.length; i++) {
+			arr[i]=sc.nextInt();
+		}
+		//array traversing
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
 }
